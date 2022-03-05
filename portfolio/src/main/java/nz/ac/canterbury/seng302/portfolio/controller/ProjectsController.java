4c3c40004bb48adcca24a1079f77d90874e3a8b7
Bede Skinner-Vennell <bjs185@uclive.ac.nz>
2022-03-05 17:34:24 +1300

    Default project is created if none exist
    Checks if there are projects in the database, if there aren't it creates one with default parameters. #feature

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/ProjectsController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/ProjectsController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
+import java.util.Calendar;
+
+import java.sql.Date;
 
 @Controller
 public class ProjectsController {
@@ -20,19 +23,24 @@ public class ProjectsController {
 
     @GetMapping("/projects")
     public String projects(Model model) {
+//        projectEntityRepository.deleteAll(); // Use for testing if default project works
         List<ProjectEntity> projects = StreamSupport.stream(projectEntityRepository.findAll().spliterator(), false).toList();
-        model.addAttribute("project1Name", projects.get(0).getProject_name());
-        String project1Start = String.format("Start Date: %s", projects.get(0).getStart_date());
-        model.addAttribute("project1Start", project1Start);
-        String project1End = String.format("End Date: %s", projects.get(0).getEnd_date());
-        model.addAttribute("project1End", project1End);
-        model.addAttribute("project1Description", projects.get(0).getDescription());
-        model.addAttribute("project2Name", projects.get(1).getProject_name());
-        String project2Start = String.format("Start Date: %s", projects.get(1).getStart_date());
-        model.addAttribute("project2Start", project2Start);
-        String project2End = String.format("End Date: %s", projects.get(1).getEnd_date());
-        model.addAttribute("project2End", project2End);
-        model.addAttribute("project2Description", projects.get(1).getDescription());
+
+        if (projects.size() < 1) {
+            Calendar cal = Calendar.getInstance();
+            String projectName = String.format("Project %d", cal.get(Calendar.YEAR));
+            Date startDate = new Date(cal.getTimeInMillis());
+            cal.add(Calendar.MONTH, 8);
+            Date endDate = new Date(cal.getTimeInMillis());
+            String description = "";
+            Long projectId = Long.valueOf(1);
+            ProjectEntity defaultProject = new ProjectEntity(projectId, projectName, description, startDate, endDate);
+            projectEntityRepository.save(defaultProject);
+            projects = StreamSupport.stream(projectEntityRepository.findAll().spliterator(), false).toList();
+        }
+
+
+        model.addAttribute("projects", projects);
 
         return "projects";
     }
