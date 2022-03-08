e049af0e60a69ad8db1ba9a260608fced9bbc1d0
Billy Sandri <bsa57@uclive.ac.nz>
2022-03-08 13:34:13 +1300

    Added test for finding all users in the database.
    #Test

--- portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/ProjectEntityRepositoryTest.java
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/ProjectEntityRepositoryTest.java
@@ -1,6 +1,9 @@
 package nz.ac.canterbury.seng302.portfolio;
 
+import nz.ac.canterbury.seng302.portfolio.entities.ProjectEntity;
 import nz.ac.canterbury.seng302.portfolio.repositories.ProjectEntityRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -8,6 +11,12 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.persistence.EntityManager;
 import javax.sql.DataSource;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.StreamSupport;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
@@ -20,11 +29,37 @@ public class ProjectEntityRepositoryTest {
     @Autowired private EntityManager entityManager;
     @Autowired private ProjectEntityRepository projectEntityRepository;
 
+    @BeforeEach
+    void cleanDatabase() {
+        projectEntityRepository.deleteAll();
+    }
+
     @Test
-    void injectedComponentsAreNotNull(){
+    void injectedComponentsAreNotNull() {
         assertThat(dataSource).isNotNull();
         assertThat(jdbcTemplate).isNotNull();
         assertThat(entityManager).isNotNull();
         assertThat(projectEntityRepository).isNotNull();
+    }
+
+    @Test
+    void findUsers() {
+        ProjectEntity project1 = new ProjectEntity(1L, "Project1", "Test Project", Date.valueOf("2022-04-15"), Date.valueOf("2022-05-16"));
+        ProjectEntity project2 = new ProjectEntity(2L, "Project1", "Test Project", Date.valueOf("2022-03-22"), Date.valueOf("2022-04-01"));
+        List<ProjectEntity> projects = new ArrayList<ProjectEntity>();
+        projects.add(project1);
+        projects.add(project2);
+        projectEntityRepository.saveAll(projects);
+        List<ProjectEntity> projectsFromDatabase = StreamSupport.stream(projectEntityRepository.findAll().spliterator(), false).toList();
+        assertThat(projectsFromDatabase.get(0).getProject_id()).isEqualTo(projects.get(0).getProject_id());
+        assertThat(projectsFromDatabase.get(0).getProject_name()).isEqualTo(projects.get(0).getProject_name());
+        assertThat(projectsFromDatabase.get(0).getDescription()).isEqualTo(projects.get(0).getDescription());
+        assertThat(projectsFromDatabase.get(0).getStart_date()).isEqualTo(projects.get(0).getStart_date());
+        assertThat(projectsFromDatabase.get(0).getEnd_date()).isEqualTo(projects.get(0).getEnd_date());
+        assertThat(projectsFromDatabase.get(1).getProject_id()).isEqualTo(projects.get(1).getProject_id());
+        assertThat(projectsFromDatabase.get(1).getProject_name()).isEqualTo(projects.get(1).getProject_name());
+        assertThat(projectsFromDatabase.get(1).getDescription()).isEqualTo(projects.get(1).getDescription());
+        assertThat(projectsFromDatabase.get(1).getStart_date()).isEqualTo(projects.get(1).getStart_date());
+        assertThat(projectsFromDatabase.get(1).getEnd_date()).isEqualTo(projects.get(1).getEnd_date());
     }
 }
