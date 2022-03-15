9facfae614759811d15e5718a3b5e9c1d7597c79
Amy Sloane <ajs418@uclive.ac.nz>
2022-03-15 14:40:41 +1300

    #fix remove test identityProvider, and replace with identityprovider,i.e. fixing the capitalisation

--- identityprovider/src/test/java/nz/ac/canterbury/seng302/identityprovider/IdentityProviderApplicationTests.java
+++ identityprovider/src/test/java/nz/ac/canterbury/seng302/identityprovider/IdentityProviderApplicationTests.java
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.Objects;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
-class IdentityProviderApplicationTests {
+class IdentityproviderApplicationTests {
 
     @Autowired
     private UserRepository repository;
