0ce822365955ee90e3a769b0368d1c95bc2219a0
Sam <smc397@uclive.ac.nz>
2022-03-17 18:25:27 +1300

    #feature #refactor #document removed weird time stamp on profile page. Added days and years to be displayed on profile for date of registration. Put into its own method for readability. Added some documentation.

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/ProfileController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/ProfileController.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
@@ -38,15 +37,34 @@ public class ProfileController {
         UserResponse user = userService.getUserAccountById(id);
         model.addAttribute("user", user);
         model.addAttribute("name", user.getFirstName() + " " + user.getLastName());
-        Timestamp ts = user.getCreated();
-        Instant timeCreated = Instant.ofEpochSecond( ts.getSeconds() , ts.getNanos() );
-        LocalDate dateCreated = timeCreated.atZone( ZoneId.systemDefault() ).toLocalDate();
-        long months = ChronoUnit.MONTHS.between(dateCreated, LocalDate.now());
-        String formattedDate = "Member Since: " + dateCreated + " (" + months + " months)";
-        model.addAttribute("date", formattedDate);
+        model.addAttribute("date", getTimeSinceCreated(user.getCreated()));
         return "profile";
     }
 
-
+    /**
+     * Method to create a string representing time since member was created
+     * @param ts Timestamp representing the user attribute timeSinceCreated
+     * @return formattedDate a string representing time since account was created to be displayed in profile
+     */
+    private String getTimeSinceCreated(Timestamp ts) {
+        Instant timeCreated = Instant.ofEpochSecond( ts.getSeconds() , ts.getNanos() );
+        LocalDate dateCreated = timeCreated.atZone( ZoneId.systemDefault() ).toLocalDate();
+        String formattedDate = "Member Since: " + dateCreated + " ";
+        long totalMonths = ChronoUnit.MONTHS.between(dateCreated, LocalDate.now());
+        if (totalMonths == 0) {
+            long days = ChronoUnit.DAYS.between(dateCreated, LocalDate.now());
+            formattedDate += "(" + days + " days)";
+        } else {
+            long months = totalMonths % 12;
+            formattedDate += "(" + months + " months";
+            long years = Math.floorDiv(totalMonths, 12);
+            if (years > 0) {
+                formattedDate += " " + years + " years)";
+             } else {
+                formattedDate += ")";
+            }
+        }
+        return formattedDate;
+    }
 }
 
