8a4c2aa6d17daf032c7a330611b959eeebf0d4fb
Billy Sandri <bsa57@uclive.ac.nz>
2022-03-17 13:16:19 +1300

    Added new controller for viewing specific project.
    #Feature

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/ProjectDetailsController.java
@@ -0,0 +1,54 @@
+package nz.ac.canterbury.seng302.portfolio.controller;
+
+import nz.ac.canterbury.seng302.portfolio.model.Project;
+import nz.ac.canterbury.seng302.portfolio.model.Sprint;
+import nz.ac.canterbury.seng302.portfolio.service.ProjectService;
+import nz.ac.canterbury.seng302.portfolio.service.SprintService;
+import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
+import nz.ac.canterbury.seng302.shared.identityprovider.ClaimDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+public class ProjectDetailsController {
+
+    @Autowired
+    private ProjectService projectService;
+    @Autowired
+    private SprintService sprintService;
+
+    @GetMapping("/projects/{id}")
+    public String projectDetails(@AuthenticationPrincipal AuthState principal, Model model, @PathVariable("id") String id) throws Exception {
+        /* Add project details to the model */
+        // Gets the project with id 0 to plonk on the page
+        int projectId = Integer.parseInt(id);
+        Project project = projectService.getProjectById(projectId);
+        model.addAttribute("project", project);
+
+        List<Sprint> sprintList = sprintService.getAllSprints();
+        model.addAttribute("sprints", sprintList);
+
+
+        // Below code is just begging to be added as a method somewhere...
+        String role = principal.getClaimsList().stream()
+                .filter(claim -> claim.getType().equals("role"))
+                .findFirst()
+                .map(ClaimDTO::getValue)
+                .orElse("NOT FOUND");
+
+        /* Return the name of the Thymeleaf template */
+        // detects the role of the current user and returns appropriate page
+        /*if (role.equals("teacher")) {
+            return "teacherProjectDetails";
+        } else {
+            return "userProjectDetails";
+        }*/
+        return "teacherProjectDetails";
+    }
+}
