e35a122c79e546e7cf22032237c5f89214849abd
Bede Skinner-Vennell <bjs185@uclive.ac.nz>
2022-03-17 14:29:58 +1300

    #refactor Got rid of repeated code in add/edit project javascript
    Also fixed a bug with the project date limits not working correctly

--- portfolio/src/main/resources/static/scripts/projects.js
+++ portfolio/src/main/resources/static/scripts/projects.js
@@ -65,21 +65,19 @@ document.addEventListener('click', (e) => {
  * @returns {string} The string formatted date (yyyy-mm-dd) extracted from the date object
  */
 function getDateString(date) {
-    let dd = date.getDate();
-    let mm = date.getMonth() + 1; //January is 0!
-    let yyyy = date.getFullYear();
+    let days = date.getDate();
+    let months = date.getMonth() + 1; //January is 0!
+    let years = date.getFullYear();
 
-    // Ensure day of month 2 characters wide
-    if (dd < 10) {
-        dd = '0' + dd;
+    // Ensure day and month are two characters wide
+    if (days < 10) {
+        days = '0' + days;
+    }
+    if (months < 10) {
+        months = '0' + months;
     }
 
-    // Ensure month of year 2 characters wide
-    if (mm < 10) {
-        mm = '0' + mm;
-    }
-
-    return yyyy + '-' + mm + '-' + dd;
+    return years + '-' + months + '-' + days;
 }
 
 
@@ -90,46 +88,100 @@ function getDateString(date) {
 function updateMinEndDate() {
     let startDate = document.getElementById("add-edit__start-date-field").valueAsNumber;
     let endDate = new Date(startDate);
-    endDate.setUTCDate(endDate.getUTCDate() + 1);
+    endDate.setDate(endDate.getDate() + 1);
 
     document.getElementById("add-edit__end-date-field").setAttribute('min', getDateString(endDate));
 }
 
-// // Whenever the start date field of the Add/Edit Project form changes, update the end date field's minimum
-// document.getElementById("add-edit__start-date-field").onchange = function () {
-//     updateMinEndDate();
-// };
+/**
+ * Parses the dates into a readable format and calls the function to fill in the edit/add form
+ * and make it visible
+ *
+ * @param project_id The project's ID
+ * @param project_name The project's name
+ * @param project_description The project's description
+ * @param start_date_string The project's start date as a string
+ * @param end_date_string The project's end date as a string
+ */
+function editProject(project_id, project_name, project_description, start_date_string, end_date_string) {
+
+    // Calculate the start and end dates and convert from UTC to NZT
+    let start_date = new Date(Date.parse(start_date_string));
+    start_date.setUTCHours(start_date.getUTCHours() - start_date.getTimezoneOffset() / 60);
+    let end_date = new Date(Date.parse(end_date_string));
+    end_date.setUTCHours(end_date.getUTCHours() - start_date.getTimezoneOffset() / 60);
+
+    let form_title = "Edit Project";
+
+    openAddEditForm(form_title, project_id, project_name, project_description, start_date, end_date);
+
+
+}
+
+/**
+ * Calculates the default project values then calls the function to fill in the edit/add form
+ * and make it visible
+ */
+function addProject() {
+
+    // Calculate the start and end dates
+    // Start date is today
+    // End date is 8 months from now
+    let start_date = new Date();
+    start_date.setUTCHours(start_date.getUTCHours() - start_date.getTimezoneOffset() / 60);
+
+    let end_date = new Date(start_date.getTime());
+    end_date.setUTCMonth(end_date.getUTCMonth() + 8);
+
+    // Assign default project values
+    let project_id = "";
+    let project_description = "";
+    let project_name = "Project " + start_date.getFullYear();
+    let form_title = "Add Project";
+
+
+    openAddEditForm(form_title, project_id, project_name, project_description, start_date, end_date);
+}
 
 
 /**
- * Opens the Add/Edit Project form, filling form inputs with the existing values of the desired project.
+ * Fills in the form elements then makes the form visible
+ * Calculates the earliest dates that dates can be set to and applies limits to those inputs
  *
- * @param id The project's id
- * @param name The project's name
- * @param description The project's description
- * @param startDateString The project's start date (Date object)
- * @param endDateString The project's end date (Date object)
+ * @param form_title The title of the form (either 'Add Project' or 'Edit Project')
+ * @param project_id The project's ID
+ * @param project_name The project's name
+ * @param project_description The project's description
+ * @param start_date The project's start date
+ * @param end_date The project's end date
  */
-function openAddEditForm(id, name, description, startDate, endDate) {
+function openAddEditForm(form_title, project_id, project_name, project_description, start_date, end_date) {
+
+    // Set the form title
+    document.getElementById("add-edit__title").innerHTML = "<h1>" + form_title + "</h1>"
+
+    // Populate form inputs with project name and description
+    document.getElementById("add-edit__name-field").value = project_name;
+    document.getElementById("add-edit__description-field").value = project_description;
+
+    // Insert start and end dates
+    document.getElementById("add-edit__start-date-field").valueAsNumber = start_date;
+    document.getElementById("add-edit__end-date-field").valueAsNumber = end_date;
+
+    // Set the id of the project being edited
+    document.getElementById("add-edit__project-id").value = project_id;
+
     // Display the form and grey out other page content using overlay
     document.getElementById("add-edit__popup").classList.remove("hidden");
     document.getElementById("add-edit__overlay").classList.remove("hidden");
 
-    // Set id of project being edited
-    document.getElementById("add-edit__project-id").value = id;
+    // Set minimum end date to the day after the current start date
+    updateMinEndDate();
 
-    // Populate form inputs with provided values
-    document.getElementById("add-edit__name-field").value = name;
-    document.getElementById("add-edit__description-field").value = description;
-
-    // Dates
-    startDate.setUTCHours(startDate.getUTCHours() + 13);
-    document.getElementById("add-edit__start-date-field").setAttribute('min', getDateString(startDate));
-    document.getElementById("add-edit__start-date-field").valueAsNumber = startDate;
-
-    endDate.setUTCHours(endDate.getUTCHours() + 13);
-    document.getElementById("add-edit__end-date-field").setAttribute('min', getDateString(endDate));
-    document.getElementById("add-edit__end-date-field").valueAsNumber = endDate;
+    // Set the minimum start date to one year before today
+    let min_date = new Date();
+    min_date.setFullYear(min_date.getFullYear() - 1);
+    document.getElementById("add-edit__start-date-field").setAttribute('min', getDateString(min_date));
 }
 
 
@@ -145,25 +197,3 @@ function closeAddEditForm() {
     // Return false to prevent form submission
     return false;
 }
-
-// // When the Add/Edit form discard button is clicked, close the form without submitting
-// document.getElementById("add-edit__discard").onclick = function () {
-//     return closeAddEditForm();
-// };
-
-/**
- * Opens the Add/Edit form for a new project, populating input fields with default project details.
- */
-function addProject() {
-    let startDate = new Date();
-    startDate.setUTCHours(startDate.getUTCHours() + 13);
-    let endDate = new Date(startDate.getTime());
-    endDate.setUTCMonth(endDate.getUTCMonth() + 8);
-
-    openAddEditForm(null, "Project " + startDate.getFullYear(), "", startDate, endDate);
-}
-
-// // When the Add Project button is clicked, open the Add/Edit form for a new project
-// document.getElementById("new-project-button").onclick = function () {
-//     addProject();
-// }
