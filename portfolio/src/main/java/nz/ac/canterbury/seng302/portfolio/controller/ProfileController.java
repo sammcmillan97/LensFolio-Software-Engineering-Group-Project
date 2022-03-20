60915498447b44ed103384e22cd9bbd223114d63
Luke Garside <lcg37@uclive.ac.nz>
2022-03-20 22:56:17 +1300

    #feature Fixed some minor issues in the display of dates on the profile page

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/ProfileController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/ProfileController.java
@@ -11,10 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 @Controller
 public class ProfileController {
@@ -48,30 +50,33 @@ public class ProfileController {
      * @return formattedDate a string representing time since account was created to be displayed in profile
      */
     private String getTimeSinceCreated(Timestamp ts) {
-        System.out.println(ts);
         Instant timeCreated = Instant.ofEpochSecond( ts.getSeconds() , ts.getNanos() );
-        LocalDate dateCreated = timeCreated.atZone( ZoneId.systemDefault() ).toLocalDate();
-        String formattedDate = "Member Since: " + dateCreated + " ";
-        long totalMonths = ChronoUnit.MONTHS.between(dateCreated, LocalDate.now());
-        System.out.println(dateCreated + " date created " + LocalDate.now() + " now");
+        LocalDate localDateCreated = timeCreated.atZone( ZoneId.systemDefault() ).toLocalDate();
+        Date dateCreated = java.util.Date.from(localDateCreated.atStartOfDay()
+                .atZone(ZoneId.systemDefault())
+                .toInstant());
+        SimpleDateFormat dateFormat = new SimpleDateFormat("d MMMMM yyyy");
+        String formattedDate = "Member Since: " + dateFormat.format(dateCreated) + " ";
+        long totalMonths = ChronoUnit.MONTHS.between(localDateCreated, LocalDate.now());
+
         long months = totalMonths % 12;
-
-        String monthPlural = " Months";
-        if(months == 1) {
-            monthPlural = " Month";
-        }
-        formattedDate += "(" + months + monthPlural;
-
         long years = Math.floorDiv(totalMonths, 12);
+
+        formattedDate += "(";
         if (years > 0) {
-            String yearPlural = " Years";
+            String yearPlural = " years";
             if (years == 1) {
-                yearPlural = " Year";
+                yearPlural = " year";
             }
-            formattedDate += " " + years + yearPlural;
-         } else {
-            formattedDate += ")";
+            formattedDate += years + yearPlural + " ";
         }
+
+        String monthPlural = " months";
+        if(months == 1) {
+            monthPlural = " month";
+        }
+        formattedDate += months + monthPlural + ")";
+
         return formattedDate;
     }
 }
