897275ed927d8a7d7b759d5a010dccc82e6368ca
Connor Dunlop <cdu51@uclive.ac.nz>
2022-03-21 07:40:34 +1300

    Added sprint styling, and collapsible sprint behaviour.

--- portfolio/src/main/resources/static/scripts/projects.js
+++ portfolio/src/main/resources/static/scripts/projects.js
@@ -47,8 +47,32 @@ document.addEventListener('click', (e) => {
     if (element.classList.contains("project__name")) {
         // Get the project element of the project who's name was clicked
         // And toggle accordion
-        let project = element.parentNode;
-        toggleAccordion(project);
+        let project = element.closest(".project");
+
+        // If collapsing project accordion
+        // Also collapse all of it's sprints
+        if (project.classList.contains("expanded")) {
+            let sprints = project.querySelectorAll(".sprint");
+            sprints.forEach(collapse);
+            collapse(project);
+        } else {
+            expand(project);
+        }
+    }
+});
+
+/**
+ * Toggle sprint accordion when sprint label is clicked
+ */
+document.addEventListener('click', (e) => {
+    let element = e.target;
+
+    // Check if a project name was clicked
+    if (element.classList.contains("sprint__name")) {
+        // Get the sprint element of the sprint who's name was clicked
+        // And toggle accordion
+        let sprint = element.closest(".sprint");
+        toggleAccordion(sprint);
     }
 });
 
