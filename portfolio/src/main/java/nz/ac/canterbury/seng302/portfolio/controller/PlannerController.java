609ca3c5d281d08a70a00b5474a31e93c8ee425b
Bede Skinner-Vennell <bjs185@uclive.ac.nz>
2022-03-25 10:06:24 +1300

    #refactor reimplemented commented out code and changed to Calendar instead of Date objects

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/PlannerController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/PlannerController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -35,25 +36,27 @@ public class PlannerController {
     public String planner(@RequestParam(name="projectId", defaultValue = "-1") int projectId,
                           Model model) {
 
-        Date startDate = new Date();
-        Date endDate = new Date();
-        endDate.setMonth(endDate.getMonth() + 8);
+        Calendar startDate = Calendar.getInstance();
+        Calendar endDate = Calendar.getInstance();
+        endDate.add(Calendar.MONTH, 8);
 
-        Project project = new Project("Default Project", "Random Description", startDate, endDate);
-        model.addAttribute("project", project);
-//        if (projectId == -1) {
-//            List<Project> projects = projectService.getAllProjects();
-//            if (projects.size() > 0) {
-//                model.addAttribute("project", projects.get(0));
-//            }
-//        } else {
-//            try {
-//                Project project = projectService.getProjectById(projectId);
-//                model.addAttribute("project", project);
-//            } catch (Exception e) {
-//
-//            }
-//        }
+
+        if (projectId == -1) {
+            List<Project> projects = projectService.getAllProjects();
+            if (projects.size() > 0) {
+                model.addAttribute("project", projects.get(0));
+            } else {
+                Project project = new Project("Default Project", "Random Description", startDate.getTime(), endDate.getTime());
+                model.addAttribute("project", project);
+            }
+        } else {
+            try {
+                Project project = projectService.getProjectById(projectId);
+                model.addAttribute("project", project);
+            } catch (Exception ignored) {
+
+            }
+        }
 
 
         return "planner";
