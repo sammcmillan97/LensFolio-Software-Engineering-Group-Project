3bb19b8a59ddee6dfa49a821a423c07d8d1c415d
Luke Garside <lcg37@uclive.ac.nz>
2022-03-29 11:44:00 +1300

    #refactor #fix Separated ProfilePictureController into its own class, instead of being in EditUserController.

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/EditUserController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/EditUserController.java
@@ -2,29 +2,20 @@ package nz.ac.canterbury.seng302.portfolio.controller;
 
 
 import com.google.protobuf.Timestamp;
-import io.grpc.StatusRuntimeException;
-import nz.ac.canterbury.seng302.portfolio.authentication.CookieUtil;
-import nz.ac.canterbury.seng302.portfolio.service.AuthenticateClientService;
 import nz.ac.canterbury.seng302.portfolio.service.UserAccountClientService;
 import nz.ac.canterbury.seng302.shared.identityprovider.*;
-import nz.ac.canterbury.seng302.shared.util.FileUploadStatusResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
-import java.util.Arrays;
 
 @Controller
 public class EditUserController {
@@ -133,105 +124,6 @@ public class EditUserController {
             model.addAttribute("editMessage", editUserResponse.getMessage());
             return "/editUser";
         }
-    }
-
-    /**
-     * Get mapping to open addProfilePicture page
-     * @param principal
-     * @param model
-     * @return the addProfilePicture page
-     */
-    @GetMapping("/addProfilePicture")
-    public String addProfilePicture(
-            @AuthenticationPrincipal AuthState principal,
-            Model model
-    ) {
-        Integer id = Integer.valueOf(principal.getClaimsList().stream()
-                .filter(claim -> claim.getType().equals("nameid"))
-                .findFirst()
-                .map(ClaimDTO::getValue)
-                .orElse("-100"));
-
-        UserResponse user = userAccountClientService.getUserAccountById(id);
-        model.addAttribute("user", user);
-        return "addProfilePicture";
-    }
-
-
-
-    @PostMapping("/addProfilePicture")
-    public String addProfilePicture(@AuthenticationPrincipal AuthState principal,
-                                    @RequestParam(name="fileContent") byte[] fileContent,
-                                    @RequestParam(name="username") String username,
-                                    @RequestParam(name="fileType") String fileType,
-                                    Model model) throws IOException {
-
-        //get userId using the Authentication Principle
-        Integer id = Integer.valueOf(principal.getClaimsList().stream()
-                .filter(claim -> claim.getType().equals("nameid"))
-                .findFirst()
-                .map(ClaimDTO::getValue)
-                .orElse("-100"));
-
-        try {
-            userAccountClientService.uploadUserProfilePhoto(fileContent, id, fileType);
-            //Get the new version of user
-            UserResponse user = userAccountClientService.getUserAccountById(id);
-            model.addAttribute("user", user);
-            model.addAttribute("username", username);
-            model.addAttribute("name", user.getFirstName() + " " + user.getLastName());
-            Timestamp ts = user.getCreated();
-            Instant timeCreated = Instant.ofEpochSecond(ts.getSeconds(), ts.getNanos());
-            LocalDate dateCreated = timeCreated.atZone(ZoneId.systemDefault()).toLocalDate();
-            long months = ChronoUnit.MONTHS.between(dateCreated, LocalDate.now());
-            String formattedDate = "Member Since: " + dateCreated + " (" + months + " months)";
-            model.addAttribute("date", formattedDate);
-            return "addProfilePicture";
-        } catch (Exception e) {
-            model.addAttribute("errorMessage", e);
-        }
-
-        return "addProfilePicture";
-    }
-
-    @DeleteMapping("/removeProfilePicture")
-    public String removeProfilePicture(@AuthenticationPrincipal AuthState principal,
-                                    @RequestParam(name="username") String username,
-                                    Model model) {
-
-        //get userId using the Authentication Principle
-        Integer id = Integer.valueOf(principal.getClaimsList().stream()
-                .filter(claim -> claim.getType().equals("nameid"))
-                .findFirst()
-                .map(ClaimDTO::getValue)
-                .orElse("-100"));
-
-        DeleteUserProfilePhotoResponse deleteUserProfilePhotoResponse;
-        deleteUserProfilePhotoResponse = userAccountClientService.deleteUserProfilePhoto(id);
-
-        //Get the new version of user
-        UserResponse user = userAccountClientService.getUserAccountById(id);
-        model.addAttribute("user", user);
-        model.addAttribute("username", username);
-
-        //if remove profile picture was successful return to profile
-        if (deleteUserProfilePhotoResponse.getIsSuccess()){
-            //generic model attribute that need to be set for the profile page
-            model.addAttribute("name", user.getFirstName() + " " + user.getLastName());
-            Timestamp ts = user.getCreated();
-            Instant timeCreated = Instant.ofEpochSecond( ts.getSeconds() , ts.getNanos() );
-            LocalDate dateCreated = timeCreated.atZone( ZoneId.systemDefault() ).toLocalDate();
-            long months = ChronoUnit.MONTHS.between(dateCreated, LocalDate.now());
-            String formattedDate = "Member Since: " + dateCreated + " (" + months + " months)";
-            model.addAttribute("date", formattedDate);
-            return "addProfilePicture";
-        } else {
-            //if edit user was unsuccessful
-            model.addAttribute("deleteMessage", "");
-            model.addAttribute("deleteMessage", deleteUserProfilePhotoResponse.getMessage());
-            return "/addProfilePicture";
-        }
-
     }
 
 }
---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/ProfilePictureController.java
@@ -0,0 +1,128 @@
+package nz.ac.canterbury.seng302.portfolio.controller;
+
+import com.google.protobuf.Timestamp;
+import nz.ac.canterbury.seng302.portfolio.service.UserAccountClientService;
+import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
+import nz.ac.canterbury.seng302.shared.identityprovider.ClaimDTO;
+import nz.ac.canterbury.seng302.shared.identityprovider.DeleteUserProfilePhotoResponse;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+
+@Controller
+public class ProfilePictureController {
+
+    @Autowired
+    private UserAccountClientService userAccountClientService;
+
+    /**
+     * Get mapping to open addProfilePicture page
+     * @param principal
+     * @param model
+     * @return the addProfilePicture page
+     */
+    @GetMapping("/addProfilePicture")
+    public String addProfilePicture(
+            @AuthenticationPrincipal AuthState principal,
+            Model model
+    ) {
+        Integer id = Integer.valueOf(principal.getClaimsList().stream()
+                .filter(claim -> claim.getType().equals("nameid"))
+                .findFirst()
+                .map(ClaimDTO::getValue)
+                .orElse("-100"));
+
+        UserResponse user = userAccountClientService.getUserAccountById(id);
+        model.addAttribute("user", user);
+        return "addProfilePicture";
+    }
+
+
+
+    @PostMapping("/addProfilePicture")
+    public String addProfilePicture(@AuthenticationPrincipal AuthState principal,
+                                    @RequestParam(name="fileContent") byte[] fileContent,
+                                    @RequestParam(name="username") String username,
+                                    @RequestParam(name="fileType") String fileType,
+                                    Model model) {
+
+        //get userId using the Authentication Principle
+        Integer id = Integer.valueOf(principal.getClaimsList().stream()
+                .filter(claim -> claim.getType().equals("nameid"))
+                .findFirst()
+                .map(ClaimDTO::getValue)
+                .orElse("-100"));
+
+        try {
+            userAccountClientService.uploadUserProfilePhoto(fileContent, id, fileType);
+            // Generic attributes that need to be set for the profile page
+            UserResponse user = userAccountClientService.getUserAccountById(id);
+            model.addAttribute("user", user);
+            model.addAttribute("name", user.getFirstName() + " " + user.getLastName());
+            Timestamp ts = user.getCreated();
+            Instant timeCreated = Instant.ofEpochSecond( ts.getSeconds() , ts.getNanos() );
+            LocalDate dateCreated = timeCreated.atZone( ZoneId.systemDefault() ).toLocalDate();
+            long months = ChronoUnit.MONTHS.between(dateCreated, LocalDate.now());
+            String formattedDate = "Member Since: " + dateCreated + " (" + months + " months)";
+            model.addAttribute("date", formattedDate);
+            return "profile";
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", e);
+        }
+
+        return "addProfilePicture";
+    }
+
+    @DeleteMapping("/removeProfilePicture")
+    public String removeProfilePicture(@AuthenticationPrincipal AuthState principal,
+                                       @RequestParam(name="username") String username,
+                                       Model model) {
+
+        //get userId using the Authentication Principle
+        Integer id = Integer.valueOf(principal.getClaimsList().stream()
+                .filter(claim -> claim.getType().equals("nameid"))
+                .findFirst()
+                .map(ClaimDTO::getValue)
+                .orElse("-100"));
+
+        DeleteUserProfilePhotoResponse deleteUserProfilePhotoResponse;
+        deleteUserProfilePhotoResponse = userAccountClientService.deleteUserProfilePhoto(id);
+
+        //Get the new version of user
+        UserResponse user = userAccountClientService.getUserAccountById(id);
+        model.addAttribute("user", user);
+        model.addAttribute("username", username);
+
+        //if remove profile picture was successful return to profile
+        if (deleteUserProfilePhotoResponse.getIsSuccess()){
+            //generic model attribute that need to be set for the profile page
+            model.addAttribute("name", user.getFirstName() + " " + user.getLastName());
+            Timestamp ts = user.getCreated();
+            Instant timeCreated = Instant.ofEpochSecond( ts.getSeconds() , ts.getNanos() );
+            LocalDate dateCreated = timeCreated.atZone( ZoneId.systemDefault() ).toLocalDate();
+            long months = ChronoUnit.MONTHS.between(dateCreated, LocalDate.now());
+            String formattedDate = "Member Since: " + dateCreated + " (" + months + " months)";
+            model.addAttribute("date", formattedDate);
+            return "addProfilePicture";
+        } else {
+            //if edit user was unsuccessful
+            model.addAttribute("deleteMessage", "");
+            model.addAttribute("deleteMessage", deleteUserProfilePhotoResponse.getMessage());
+            return "/addProfilePicture";
+        }
+
+    }
+
+}
