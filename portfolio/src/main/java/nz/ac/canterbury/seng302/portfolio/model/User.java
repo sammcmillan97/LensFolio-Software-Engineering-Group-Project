f2eec14798c92cc50edd132cd8d72180887f2ed4
Luke Garside <lcg37@uclive.ac.nz>
2022-04-14 12:25:36 +1200

    #feature Created user class in portfolio.

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/User.java
@@ -0,0 +1,85 @@
+package nz.ac.canterbury.seng302.portfolio.model;
+
+import com.google.protobuf.Timestamp;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserResponse;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserRole;
+
+import java.util.Collection;
+
+/**
+ * Representation of a user for use in portfolio.
+ * Only contains methods for what the portfolio currently uses.
+ * These should be added to if more functionality is needed.
+ */
+public class User {
+
+    private String username;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String nickname;
+    private String bio;
+    private String personalPronouns;
+    private String email;
+    private Collection<UserRole> roles;
+    private Timestamp timeCreated;
+    private String profileImagePath;
+
+    public User(UserResponse source) {
+        username = source.getUsername();
+        firstName = source.getFirstName();
+        middleName = source.getMiddleName();
+        lastName = source.getLastName();
+        nickname = source.getNickname();
+        bio = source.getBio();
+        personalPronouns = source.getPersonalPronouns();
+        email = source.getEmail();
+        roles = source.getRolesList();
+        timeCreated = source.getCreated();
+        profileImagePath = source.getProfileImagePath();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public String getPersonalPronouns() {
+        return personalPronouns;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Collection<UserRole> getRoles() {
+        return roles;
+    }
+
+    public Timestamp getTimeCreated() {
+        return timeCreated;
+    }
+
+    public String getProfileImagePath() {
+        return profileImagePath;
+    }
+}
