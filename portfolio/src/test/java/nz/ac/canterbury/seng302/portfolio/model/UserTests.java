98d0c98349090ff98a975e141016df7cd00bd79d
Luke Garside <lcg37@uclive.ac.nz>
2022-04-15 22:19:37 +1200

    #test Added tests for getMemberSince method.

--- portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/model/UserTests.java
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/model/UserTests.java
@@ -7,7 +7,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.ArrayList;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -109,6 +114,81 @@ class UserTests {
         expectedRoles.add("Student");
         expectedRoles.add("Teacher");
         assertEquals(expectedRoles, testUser.getRoleStrings());
+    }
+
+    // Test that when a user has just been created the end of getMemberSince says 0 months
+    @Test
+    void testGetMemberSinceWithNewUser() {
+        Instant time = Instant.now();
+        Timestamp timestamp = Timestamp.newBuilder().setSeconds(time.getEpochSecond()).setNanos(time.getNano()).build();
+        UserResponse source = UserResponse.newBuilder()
+                .setCreated(timestamp).build();
+        User testUser = new User(source);
+
+        Date dateCreated = java.util.Date.from(time);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("d MMMMM yyyy");
+        String expected = "Member Since: " + dateFormat.format(dateCreated) + " (0 months)";
+        assertEquals(expected, testUser.getMemberSince());
+    }
+
+    // Test that when a user is one month old the end of getMemberSince says 1 month
+    @Test
+    void testGetMemberSinceWithOneMonth() {
+        Instant time = Instant.now().minus(40, ChronoUnit.DAYS);
+        Timestamp timestamp = Timestamp.newBuilder().setSeconds(time.getEpochSecond()).setNanos(time.getNano()).build();
+        UserResponse source = UserResponse.newBuilder()
+                .setCreated(timestamp).build();
+        User testUser = new User(source);
+
+        Date dateCreated = java.util.Date.from(time);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("d MMMMM yyyy");
+        String expected = "Member Since: " + dateFormat.format(dateCreated) + " (1 month)";
+        assertEquals(expected, testUser.getMemberSince());
+    }
+
+    // Test that when a user is three months old the end of getMemberSince says 3 months
+    @Test
+    void testGetMemberSinceWithThreeMonths() {
+        Instant time = Instant.now().minus(100, ChronoUnit.DAYS);
+        Timestamp timestamp = Timestamp.newBuilder().setSeconds(time.getEpochSecond()).setNanos(time.getNano()).build();
+        UserResponse source = UserResponse.newBuilder()
+                .setCreated(timestamp).build();
+        User testUser = new User(source);
+
+        Date dateCreated = java.util.Date.from(time);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("d MMMMM yyyy");
+        String expected = "Member Since: " + dateFormat.format(dateCreated) + " (3 months)";
+        assertEquals(expected, testUser.getMemberSince());
+    }
+
+    // Test that when a user is one year old the end of getMemberSince says 1 year 0 months
+    @Test
+    void testGetMemberSinceWithOneYear() {
+        Instant time = Instant.now().minus(370, ChronoUnit.DAYS);
+        Timestamp timestamp = Timestamp.newBuilder().setSeconds(time.getEpochSecond()).setNanos(time.getNano()).build();
+        UserResponse source = UserResponse.newBuilder()
+                .setCreated(timestamp).build();
+        User testUser = new User(source);
+
+        Date dateCreated = java.util.Date.from(time);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("d MMMMM yyyy");
+        String expected = "Member Since: " + dateFormat.format(dateCreated) + " (1 year 0 months)";
+        assertEquals(expected, testUser.getMemberSince());
+    }
+
+    // Test that when a user is two years and one month old the end of getMemberSince says 2 years 1 month
+    @Test
+    void testGetMemberSinceWithTwoYearsOneMonth() {
+        Instant time = Instant.now().minus(770, ChronoUnit.DAYS);
+        Timestamp timestamp = Timestamp.newBuilder().setSeconds(time.getEpochSecond()).setNanos(time.getNano()).build();
+        UserResponse source = UserResponse.newBuilder()
+                .setCreated(timestamp).build();
+        User testUser = new User(source);
+
+        Date dateCreated = java.util.Date.from(time);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("d MMMMM yyyy");
+        String expected = "Member Since: " + dateFormat.format(dateCreated) + " (2 years 1 month)";
+        assertEquals(expected, testUser.getMemberSince());
     }
 
 }
