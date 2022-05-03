0d823ae62b8eb93ccf3d386c0b16d0518100541e
Luke Garside <lcg37@uclive.ac.nz>
2022-05-03 13:59:38 +1200

    #feature Created a service to access the new PortfolioUserRepository with.

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/PortfolioUserService.java
@@ -0,0 +1,54 @@
+package nz.ac.canterbury.seng302.portfolio.service;
+
+import nz.ac.canterbury.seng302.portfolio.model.PortfolioUser;
+import nz.ac.canterbury.seng302.portfolio.model.PortfolioUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PortfolioUserService {
+
+    @Autowired
+    private PortfolioUserRepository repository;
+
+    /**
+     * Gets a user by their id. Creates a default user with that id if none exists.
+     * @param id The user's id from the identity provider
+     * @return The user's portfolio information
+     */
+    public PortfolioUser getUserById(int id) {
+        PortfolioUser user = repository.findByUserId(id);
+        if(user == null) {
+            return user;
+        }
+        else
+        {
+            PortfolioUser newUser = new PortfolioUser(id, "default"); // TODO change default to the true default
+            repository.save(newUser);
+            return newUser;
+        }
+    }
+
+    /**
+     * Gets a user's user list sort type. This is for the user list page.
+     * Creates a default user with that id if none exists.
+     * @param id The user's id from the identity provider
+     * @return The user's current user list sort type
+     */
+    public String getUserListSortType(int id) {
+        PortfolioUser user = getUserById(id);
+        return user.getUserListSortType();
+    }
+
+    /**
+     * Sets a user's user list sort type. This is for the user list page.
+     * Creates a default user with that id if none exists.
+     * @param id The user's id from the identity provider
+     * @param userListSortType The sort type to change to
+     */
+    public void setUserListSortType(int id, String userListSortType) {
+        PortfolioUser user = getUserById(id);
+        user.setUserListSortType(userListSortType);
+        repository.save(user);
+    }
+}
