5836f35a4f5d9f48371844fa34e478378ce0eaf2
Connor Dunlop <cdu51@uclive.ac.nz>
2022-05-06 13:41:51 +1200

    Removed context path property from server start scripts

--- runner/staging-identityprovider.sh
+++ runner/staging-identityprovider.sh
@@ -1,6 +1,5 @@
 fuser -k 9500/tcp || true
 export $(cat staging-identityprovider/.env | xargs -d '\n')
 SPRING_PROFILES_ACTIVE=deploy java -jar staging-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
-    --server.servlet.context-path=/test/identity \
     --spring.application.name=identity-provider \
     --grpc.server.port=9500
