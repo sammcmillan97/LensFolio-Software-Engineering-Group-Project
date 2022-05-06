9155a452cfeddd9faf3ad5d582a7a47c544efcae
Connor Dunlop <cdu51@uclive.ac.nz>
2022-05-06 15:28:46 +1200

    Removed servlet paths

--- runner/production-identityprovider.sh
+++ runner/production-identityprovider.sh
@@ -1,6 +1,5 @@
 fuser -k 10500/tcp || true
 export $(cat production-identityprovider/.env | xargs -d '\n')
 SPRING_PROFILES_ACTIVE=$SPRING_PROFILES_ACTIVE java -jar production-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
-    --spring.mvc.servlet.path=/prod/identity
     --spring.application.name=identity-provider \
     --grpc.server.port=10500
--- runner/production-portfolio.sh
+++ runner/production-portfolio.sh
@@ -1,7 +1,6 @@
 fuser -k 10501/tcp || true
 export $(cat production-portfolio/.env | xargs -d '\n')
 SPRING_PROFILES_ACTIVE=$SPRING_PROFILES_ACTIVE java -jar production-portfolio/libs/portfolio-0.0.1-SNAPSHOT.jar \
-    --spring.mvc.servlet.path=/prod/portfolio \
     --server.port=10501 \
     --spring.application.name=portfolio \
     --grpc.client.identity-provider-grpc-server.address=static://127.0.0.1:10500 \
