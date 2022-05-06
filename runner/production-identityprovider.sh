777dab846b62f10a340fd635acc654ba6639fcbc
Connor Dunlop <cdu51@uclive.ac.nz>
2022-05-06 19:49:09 +1200

    Revert "Test combination of servlet and context path"
    This reverts commit 5de696a1e50ef50d1c4bee8262db7d76c0471e3a.

--- runner/production-identityprovider.sh
+++ runner/production-identityprovider.sh
@@ -2,6 +2,5 @@ fuser -k 10500/tcp || true
 fuser -k 8080/tcp || true
 export $(cat production-identityprovider/.env | xargs -d '\n')
 java -jar production-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
-    --spring.mvc.servlet.path=/prod/identity \
     --spring.application.name=identity-provider \
     --grpc.server.port=10500
--- runner/staging-portfolio.sh
+++ runner/staging-portfolio.sh
@@ -1,7 +1,6 @@
 fuser -k 9501/tcp || true
 export $(cat staging-portfolio/.env | xargs -d '\n')
 java -jar staging-portfolio/libs/portfolio-0.0.1-SNAPSHOT.jar \
-    --spring.mvc.servlet.path=/test/portfolio \
     --server.port=9501 \
     --spring.application.name=portfolio \
     --grpc.client.identity-provider-grpc-server.address=static://127.0.0.1:9500 \
