dbe7eafe3cb6389e85d4fa1ab0151ebd0a8e7839
Connor Dunlop <cdu51@uclive.ac.nz>
2022-05-06 16:10:24 +1200

    fix for idp server port locked up production

--- runner/production-identityprovider.sh
+++ runner/production-identityprovider.sh
@@ -1,4 +1,5 @@
 fuser -k 10500/tcp || true
+fuser -k 8080/tcp || true
 export $(cat production-identityprovider/.env | xargs -d '\n')
 java -jar production-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
     --spring.application.name=identity-provider \
