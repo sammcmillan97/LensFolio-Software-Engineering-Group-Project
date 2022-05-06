a1f56e7c239d9a2a3dcc586e79ccec3e60a3a0d6
Connor Dunlop <cdu51@uclive.ac.nz>
2022-05-06 15:13:51 +1200

    Switch to servlet path instead of context path

--- runner/production-portfolio.sh
+++ runner/production-portfolio.sh
@@ -1,7 +1,7 @@
 fuser -k 10501/tcp || true
 export $(cat production-portfolio/.env | xargs -d '\n')
 SPRING_PROFILES_ACTIVE=$SPRING_PROFILES_ACTIVE java -jar production-portfolio/libs/portfolio-0.0.1-SNAPSHOT.jar \
-    --server.servlet.context-path=/prod/portfolio \
+    --spring.mvc.servlet.path=/prod/portfolio \
     --server.port=10501 \
     --spring.application.name=portfolio \
     --grpc.client.identity-provider-grpc-server.address=static://127.0.0.1:10500 \
