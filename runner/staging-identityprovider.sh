3d3ba0c5bf36b5193973742d8dfa4cc8823ad812
Connor Dunlop <cdu51@uclive.ac.nz>
2022-05-06 15:47:49 +1200

    Removed redundant environment variable

--- runner/staging-identityprovider.sh
+++ runner/staging-identityprovider.sh
@@ -1,5 +1,5 @@
 fuser -k 9500/tcp || true
 export $(cat staging-identityprovider/.env | xargs -d '\n')
-SPRING_PROFILES_ACTIVE=deploy java -jar staging-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
+java -jar staging-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
     --spring.application.name=identity-provider \
     --grpc.server.port=9500
