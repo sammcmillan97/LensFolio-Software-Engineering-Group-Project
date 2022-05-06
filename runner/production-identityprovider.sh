a3108eefc56c816539eab1d84526b8ccd948448c
Connor Dunlop <cdu51@uclive.ac.nz>
2022-05-06 12:53:07 +1200

    Added context path for relative urls

--- runner/production-identityprovider.sh
+++ runner/production-identityprovider.sh
@@ -1,4 +1,7 @@
 fuser -k 10500/tcp || true
 export $(cat production-identityprovider/.env | xargs -d '\n')
-SPRING_PROFILES_ACTIVE=$SPRING_PROFILES_ACTIVE java -jar production-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar --spring.application.name=identity-provider --grpc.server.port=10500
+SPRING_PROFILES_ACTIVE=$SPRING_PROFILES_ACTIVE java -jar production-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
+    --server.contextPath=/prod/identity
+    --spring.application.name=identity-provider \
+    --grpc.server.port=10500
 env
