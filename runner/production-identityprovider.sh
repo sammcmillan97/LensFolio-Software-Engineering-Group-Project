5de696a1e50ef50d1c4bee8262db7d76c0471e3a
Connor Dunlop <cdu51@uclive.ac.nz>
2022-05-06 17:18:39 +1200

    Test combination of servlet and context path

--- runner/production-identityprovider.sh
+++ runner/production-identityprovider.sh
@@ -2,5 +2,6 @@ fuser -k 10500/tcp || true
 fuser -k 8080/tcp || true
 export $(cat production-identityprovider/.env | xargs -d '\n')
 java -jar production-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
+    --spring.mvc.servlet.path=/prod/identity \
     --spring.application.name=identity-provider \
     --grpc.server.port=10500
--- runner/staging-identityprovider.sh
+++ runner/staging-identityprovider.sh
@@ -2,5 +2,6 @@ fuser -k 9500/tcp || true
 fuser -k 8080/tcp || true
 export $(cat staging-identityprovider/.env | xargs -d '\n')
 java -jar staging-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
+    --spring.mvc.servlet.path=/test/identity \
     --spring.application.name=identity-provider \
     --grpc.server.port=9500
