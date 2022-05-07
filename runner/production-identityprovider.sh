c4aa30aa572c8e87c41903a8e033531ad98940c9
Connor Dunlop <cdu51@uclive.ac.nz>
2022-05-07 17:43:23 +1200

    Made idp server ports distinct between prod and test

--- runner/production-identityprovider.sh
+++ runner/production-identityprovider.sh
@@ -1,6 +1,7 @@
 fuser -k 10500/tcp || true
-#fuser -k 8080/tcp || true
+fuser -k 10080/tcp || true
 export $(cat production-identityprovider/.env | xargs -d '\n')
 java -jar production-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
+    --server.port=10080 \
     --spring.application.name=identity-provider \
     --grpc.server.port=10500
