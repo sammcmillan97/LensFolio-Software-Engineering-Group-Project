821d30845577cdb40d26ad6d5068191590b1d34b
Connor Dunlop <cdu51@uclive.ac.nz>
2022-05-08 16:50:11 +1200

    Added thymeleaf url prefix set by environment variable

--- runner/production-portfolio.sh
+++ runner/production-portfolio.sh
@@ -1,6 +1,6 @@
 fuser -k 10501/tcp || true
 export $(cat production-portfolio/.env | xargs -d '\n')
-java -jar production-portfolio/libs/portfolio-0.0.1-SNAPSHOT.jar \
+WEB_CONTEXT=/prod/portfolio java -jar production-portfolio/libs/portfolio-0.0.1-SNAPSHOT.jar \
     --server.port=10501 \
     --spring.application.name=portfolio \
     --grpc.client.identity-provider-grpc-server.address=static://127.0.0.1:10500 \
--- runner/staging-identityprovider.sh
+++ runner/staging-identityprovider.sh
@@ -1,7 +1,7 @@
 fuser -k 9500/tcp || true
 fuser -k 9080/tcp || true
 export $(cat staging-identityprovider/.env | xargs -d '\n')
-java -jar staging-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
+WEB_CONTEXT=/test/identity java -jar staging-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
     --server.port=9080 \
     --spring.application.name=identity-provider \
     --grpc.server.port=9500
