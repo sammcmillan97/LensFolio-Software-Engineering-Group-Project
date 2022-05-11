67cf43efb49a0ad87d69a53478cea7e59ecf8785
Sam McMillan <smc397@uclive.ac.nz>
2022-05-11 12:19:28 +1200

    #document added documentation to script in addeditevent

--- portfolio/src/main/resources/static/scripts/addEditEvent.js
+++ portfolio/src/main/resources/static/scripts/addEditEvent.js
@@ -5,6 +5,10 @@
  */
 const maxNumOfChars = 30;
 
+/**
+ * Simple function that updates char_count field to reflect how many characters are remaining for the
+ * user to use
+ */
 function countCharacters() {
     let numOfEnteredChars = document.getElementById("event-form__name-field").value.length;
     let characterCounter = document.getElementById("char_count")
