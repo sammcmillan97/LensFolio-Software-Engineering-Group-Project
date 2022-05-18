b2fa50dc2ac909d638d065c1b8a7f5b8823c06b4
Amy Sloane <ajs418@uclive.ac.nz>
2022-05-19 00:27:28 +1200

    #Fix GRPC wasnt working at all so have returned to 9500

--- runner/staging-identityprovider.sh
+++ runner/staging-identityprovider.sh
@@ -4,4 +4,4 @@ export $(cat staging-identityprovider/.env | xargs -d '\n')
 WEB_CONTEXT=/test/identity java -jar staging-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
     --server.port=9080 \
     --spring.application.name=identity-provider \
-    --grpc.server.port=9502
+    --grpc.server.port=9500
