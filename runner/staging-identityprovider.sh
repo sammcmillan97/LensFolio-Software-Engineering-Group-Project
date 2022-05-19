331fff6557e61a8502acfc35bb5707cc9c47d9eb
Amy Sloane <ajs418@uclive.ac.nz>
2022-05-19 15:27:16 +1200

    #Fix replaced an additional two 9500 with port 9502

--- runner/staging-identityprovider.sh
+++ runner/staging-identityprovider.sh
@@ -1,4 +1,4 @@
-fuser -k 9500/tcp || true
+fuser -k 9502/tcp || true
 fuser -k 9080/tcp || true
 export $(cat staging-identityprovider/.env | xargs -d '\n')
 WEB_CONTEXT=/test/identity java -jar staging-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
