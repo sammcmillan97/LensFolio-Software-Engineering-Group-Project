fa6e775f4710b1729e7fe20140e51f020355a2cb
Bede Skinner-Vennell <bjs185@uclive.ac.nz>
2022-05-20 16:13:46 +1200

    #feature added blank userGroups and teacherGroups pages
    Created controller and made it direct to either userGroups or teacherGroups depending on the logged in user. Also added groups page to navbar.

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GroupsController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GroupsController.java
@@ -1,13 +1,34 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
+import nz.ac.canterbury.seng302.portfolio.model.User;
+import nz.ac.canterbury.seng302.portfolio.service.UserAccountClientService;
 import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class GroupsController {
+    @Autowired
+    private UserAccountClientService userAccountClientService;
+
+    /**
+     * Get mapping to fetch groups page
+     * @param principal Authentication principal storing current user information
+     * @param model Parameters sent to thymeleaf template to be rendered into HTML
+     * @return The user or teacher groups html page depending on the logged in user
+     */
     @GetMapping("/groups")
     public String groups(@AuthenticationPrincipal AuthState principal, Model model){
-
+        int id = userAccountClientService.getUserId(principal);
+        User user = userAccountClientService.getUserAccountById(id);
+        model.addAttribute("user", user);
+        if (userAccountClientService.isTeacher(principal)) {
+            return "teacherGroups";
+        } else {
+            return "userGroups";
+        }
     }
 }
