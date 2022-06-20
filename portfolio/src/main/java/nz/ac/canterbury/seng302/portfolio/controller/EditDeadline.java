391a2c97b9cee462c4a96b61c8b4b5b31a433e0d
Sam <smc397@uclive.ac.nz>
2022-06-21 11:16:52 +1200

    #Feature added deadline controller for displaying and handling adding/editing deadline requests. Started repurposing event/sprint form for deadlines. Added a more general css for all important date forms.

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/EditDeadline.java
@@ -0,0 +1,68 @@
+package nz.ac.canterbury.seng302.portfolio.controller;
+
+import nz.ac.canterbury.seng302.portfolio.model.Deadline;
+import nz.ac.canterbury.seng302.portfolio.model.Project;
+import nz.ac.canterbury.seng302.portfolio.model.User;
+import nz.ac.canterbury.seng302.portfolio.service.DeadlineService;
+import nz.ac.canterbury.seng302.portfolio.service.ProjectService;
+import nz.ac.canterbury.seng302.portfolio.service.UserAccountClientService;
+import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
+import nz.ac.canterbury.seng302.shared.identityprovider.ClaimDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * Controller for adding/editing deadlines
+ */
+@Controller
+public class EditDeadline {
+
+    @Autowired
+    UserAccountClientService userAccountClientService;
+
+    @Autowired
+    ProjectService projectService;
+
+    @Autowired
+    DeadlineService deadlineService;
+
+    private String timeFormat = "yyyy-MM-dd'T'HH:mm";
+    private String redirectToProjects = "redirect:/projects";
+
+
+    @GetMapping("/editDeadline-{deadlineId}-{parentProjectId}")
+    public String deadLineForm(@AuthenticationPrincipal AuthState principal,
+                            @PathVariable("parentProjectId") String parentProjectId,
+                            @PathVariable("deadlineId") String deadlineId,
+                            Model model) throws Exception {
+        if (!userAccountClientService.isTeacher(principal)) {
+            return redirectToProjects;
+        }
+        int userId = Integer.parseInt(principal.getClaimsList().stream()
+                .filter(claim -> claim.getType().equals("nameid"))
+                .findFirst()
+                .map(ClaimDTO::getValue)
+                .orElse("-100"));
+        User user = userAccountClientService.getUserAccountById(userId);
+        model.addAttribute("user", user);
+
+        int projectId = Integer.parseInt(parentProjectId);
+        Project project = projectService.getProjectById(projectId);
+        model.addAttribute("projectId", project.getId());
+
+        Deadline deadline;
+        if (Integer.parseInt(deadlineId) != -1) {
+            deadline = deadlineService.getDeadlineById(Integer.parseInt(deadlineId));
+        } else {
+            deadline = new Deadline(projectId, "New Deadline", project.getEndDate());
+        }
+        model.addAttribute("deadline", deadline);
+        model.addAttribute("minDeadlineDate", Project.dateToString(project.getStartDate(), timeFormat));
+        model.addAttribute("maxDeadlineDate", Project.dateToString(project.getEndDate(), timeFormat));
+        return "editDeadline";
+    }
+}
