24d6bc0ba1ab5e544c572ef0054565032906079f
Amy Sloane <ajs418@uclive.ac.nz>
2022-07-11 16:36:50 +1200

    #Fix Added fix for the idp - trying to get the idp running on the correct port

--- runner/production-identityprovider.sh
+++ runner/production-identityprovider.sh
@@ -1,7 +1,7 @@
 fuser -k 10500/tcp || true
-fuser -k 10080/tcp || true
+fuser -k 10502/tcp || true
 export $(cat production-identityprovider/.env | xargs -d '\n')
 WEB_CONTEXT=/prod/identity java -jar production-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
-    --server.port=10080 \
+    --server.port=10500 \
     --spring.application.name=identity-provider \
-    --grpc.server.port=10500
+    --grpc.server.port=10502
