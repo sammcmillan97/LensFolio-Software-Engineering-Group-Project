8814c7b3ce4b5dd8ae8d07cd25ef617b6ad9b1f8
Sam <smc397@uclive.ac.nz>
2022-07-14 10:33:55 +1200

    #feature #pair [smc397, lcg37] Attempting to add tooltips to planner icons. Trying different strategies

--- portfolio/src/main/resources/static/scripts/planner.js
+++ portfolio/src/main/resources/static/scripts/planner.js
@@ -42,6 +42,10 @@ function isAdmin() {
     return isTeacherOrAdmin;
 }
 
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
 /**
  * Adds an event listener to the page loading to create the calendar and set it to the project dates
  */
@@ -94,6 +98,7 @@ document.addEventListener('DOMContentLoaded', function() {
         editable: (isAdmin()),
         //disallow dragging entire event
         eventStartEditable: false,
+        // lazyFetching: false,
         showNonCurrentDates: false,
         //allow resizing of start/end date
         eventResizableFromStart: true,
@@ -111,7 +116,7 @@ document.addEventListener('DOMContentLoaded', function() {
         //Changes the html of events, deadlines and milestones so they can be represented as icons on the planner
         eventDidMount : function(info) {
             if (info.event.extendedProps.eventType === "daily-milestone") {
-                info.el.innerHTML = `<col><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trophy-fill" viewBox="0 0 16 16">
+                info.el.innerHTML = `<col data-toggle="tooltip" data-placement="top" title="Tooltip on top"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trophy-fill" viewBox="0 0 16 16">
                                      <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935z"/>
                                      </svg></col> <col> ${info.event.title} </col>`
             }
