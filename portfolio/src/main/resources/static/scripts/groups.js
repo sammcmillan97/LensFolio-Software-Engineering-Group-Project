8d907e94e18ad0a7d68480146a3e26ef6fb14368
Bede Skinner-Vennell <bjs185@uclive.ac.nz>
2022-07-13 14:57:40 +1200

    #document added comments to javascript for group page

--- portfolio/src/main/resources/static/scripts/groups.js
+++ portfolio/src/main/resources/static/scripts/groups.js
@@ -8,6 +8,10 @@ document.onselectstart = function() {
     return false;
 }
 
+/**
+ * Key released event listener
+ * Marks control/command or shift pressed variables as false if they've been released
+ */
 document.addEventListener("keyup", function (event) {
     if (event.key === "Control" || event.key === "Meta") {
         ctrlPressed = false;
@@ -16,6 +20,11 @@ document.addEventListener("keyup", function (event) {
     }
 }, false)
 
+/**
+ * Key pressed down event listener
+ * Marks the control/command or shift pressed variables as true if they've been pressed
+ * If control/command is down and A is pressed, it selects all in the current table
+ */
 document.addEventListener("keydown", function (event) {
     if (event.key === "Control" || event.key === "Meta") {
         ctrlPressed = true;
@@ -28,24 +37,37 @@ document.addEventListener("keydown", function (event) {
     }
 }, false)
 
+/**
+ * Occurs when a row is clicked on
+ * Takes the clicked on row, checks if Control or Shift are being held and performs the relevant selection action
+ * @param currRow the row that was clicked on
+ */
 function rowClick(currRow) {
+    // Set the current table to the one that the clicked on row belongs to
     currentTable = currRow.parentNode.getElementsByTagName("tr")
-    console.log(currentTable)
-    if (ctrlPressed) {
+    if (ctrlPressed) { // Toggle the clicked on row if Control is pressed
         toggleRow(currRow)
-    } else if (shiftPressed) {
+    } else if (shiftPressed) { // Select all rows between (inclusive) the current and last clicked on rows
         selectRowsBetween([lastRow.rowIndex, currRow.rowIndex])
-    } else {
+    } else { // Otherwise, clear any other selected rows and mark the clicked on row
         clearTableSelection()
         toggleRow(currRow)
     }
 }
 
+/**
+ * Takes a row and toggles it from selected to unselected, or from unselected to selected
+ * @param row the row to have selection toggled
+ */
 function toggleRow(row) {
     row.className = row.className === 'selected' ? 'unselected' : 'selected';
     lastRow = row;
 }
 
+/**
+ * Takes a list of two indexes, sorts them, then marks all rows of the table between (inclusive) those indices as selected
+ * @param indexes two table indexes in random order
+ */
 function selectRowsBetween(indexes) {
     indexes.sort(function(a, b) {
         return a - b;
@@ -56,12 +78,18 @@ function selectRowsBetween(indexes) {
     }
 }
 
+/**
+ * Unselects all items in the current table as unselected
+ */
 function clearTableSelection() {
     for (let i = 0; i < currentTable.length; i++) {
         currentTable[i].className = 'unselected';
     }
 }
 
+/**
+ * Selects all items in the current table
+ */
 function selectAllInTable() {
     for (let i = 0; i < currentTable.length; i++) {
         currentTable[i].className = 'selected';
