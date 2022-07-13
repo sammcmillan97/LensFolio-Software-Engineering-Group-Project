8e5f76f350f82cd4ae349dd723482aa68da437a7
Amy Sloane <ajs418@uclive.ac.nz>
2022-07-13 19:19:01 +1200

    #Chore trialling the ports as used 2 months ago - to attempt to fix grpc on prod

--- runner/production-identityprovider.sh
+++ runner/production-identityprovider.sh
@@ -1,7 +1,7 @@
 fuser -k 10500/tcp || true
-fuser -k 10503/tcp || true
+fuser -k 10080/tcp || true
 export $(cat production-identityprovider/.env | xargs -d '\n')
 WEB_CONTEXT=/prod/identity java -jar production-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar \
-    --server.port=10500 \
+    --server.port=10080 \
     --spring.application.name=identity-provider \
-    --grpc.server.port=10503
+    --grpc.server.port=10500
