778a5bf75760e7f1ef4c5c3972886cb028a3c7f4
Amy Sloane <ajs418@uclive.ac.nz>
2022-07-13 18:58:56 +1200

    #Fix Trialling a different port for production grpc - as it is not working

--- runner/production-portfolio.sh
+++ runner/production-portfolio.sh
@@ -3,7 +3,7 @@ export $(cat production-portfolio/.env | xargs -d '\n')
 WEB_CONTEXT=/prod/portfolio java -jar production-portfolio/libs/portfolio-0.0.1-SNAPSHOT.jar \
     --server.port=10501 \
     --spring.application.name=portfolio \
-    --grpc.client.identity-provider-grpc-server.address=static://127.0.0.1:10502 \
+    --grpc.client.identity-provider-grpc-server.address=static://127.0.0.1:10503 \
     --grpc.client.identity-provider-grpc-server.enableKeepAlive=true \
     --grpc.client.identity-provider-grpc-server.keepAliveWithoutCalls=true \
     --grpc.client.identity-provider-grpc-server.negotiationType=plaintext
