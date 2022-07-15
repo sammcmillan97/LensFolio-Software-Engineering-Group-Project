5b3a05061457c3ee45c5a709c1901d600640a47e
Amy Sloane <ajs418@uclive.ac.nz>
2022-07-15 15:55:48 +1200

    #Feature Implement a new method to retrieve all users to be used in other methods getGrouplessGroup and getTeacherGroup

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GroupsController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GroupsController.java
@@ -3,6 +3,7 @@ package nz.ac.canterbury.seng302.portfolio.controller;
 import nz.ac.canterbury.seng302.portfolio.model.Group;
 import nz.ac.canterbury.seng302.portfolio.model.GroupListResponse;
 import nz.ac.canterbury.seng302.portfolio.model.User;
+import nz.ac.canterbury.seng302.portfolio.model.UserListResponse;
 import nz.ac.canterbury.seng302.portfolio.service.GroupsClientService;
 import nz.ac.canterbury.seng302.portfolio.service.UserAccountClientService;
 import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
@@ -13,7 +14,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 public class GroupsController {
@@ -38,5 +41,49 @@ public class GroupsController {
         model.addAttribute("groups", groups.getGroups());
         System.out.println(groups.getGroups().get(1).getMembers());
         return "groups";
+    }
+
+    @GetMapping("/getGrouplessGroup")
+    public String getGrouplessGroup(@AuthenticationPrincipal AuthState principal, Model model){
+        int id = userAccountClientService.getUserId(principal);
+        User user = userAccountClientService.getUserAccountById(id);
+        model.addAttribute("user", user);
+        model.addAttribute("userIsTeacher", userAccountClientService.isTeacher(principal));
+
+        return "";
+    }
+
+    @GetMapping("/getTeacherGroup")
+    public String getTeacherGroup(@AuthenticationPrincipal AuthState principal, Model model){
+        int id = userAccountClientService.getUserId(principal);
+        User user = userAccountClientService.getUserAccountById(id);
+        model.addAttribute("user", user);
+        model.addAttribute("userIsTeacher", userAccountClientService.isTeacher(principal));
+
+        return "";
+    }
+
+    /**
+     * Retrieve all users
+     * @return set of all Users
+     */
+    protected Set<User> getAllUsers() {
+        Set<User> users = new HashSet<>();
+        int offset = 0;
+        int limit = 50;
+        int sizeOfSet = 1;
+
+        while (sizeOfSet > 0) {
+            UserListResponse userListResponse = userAccountClientService.getPaginatedUsers(offset, limit, "username", true);
+            List<User> returnedUsers = userListResponse.getUsers();
+            for(User user: returnedUsers){
+                users.add(user);
+            }
+            sizeOfSet = userListResponse.getResultSetSize();
+            offset += 50;
+            limit += 50;
+        }
+
+        return users;
     }
 }
