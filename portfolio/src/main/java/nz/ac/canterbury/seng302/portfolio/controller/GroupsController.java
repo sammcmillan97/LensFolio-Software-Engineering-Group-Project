71c38c657ac27966fe7c39ece1b956232756ba40
Amy Sloane <ajs418@uclive.ac.nz>
2022-07-15 17:41:52 +1200

    #Feature Minor fixes to new method that retrieves all users

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GroupsController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GroupsController.java
@@ -38,7 +38,7 @@ public class GroupsController {
         model.addAttribute("user", user);
         model.addAttribute("userIsTeacher", userAccountClientService.isTeacher(principal));
         GroupListResponse groupListResponse = groupsClientService.getAllGroups();
-        Set<Group> groups = (Set<Group>) groupListResponse.getGroups();
+        List<Group> groups = groupListResponse.getGroups();
         groups.add(getGrouplessGroup());
         groups.add(getTeacherGroup());
         model.addAttribute("groups", groups);
@@ -46,13 +46,13 @@ public class GroupsController {
         return "groups";
     }
 
-    public Group getGrouplessGroup(){
+    protected Group getGrouplessGroup(){
         Set<User> allUsers = getAllUsers();
 
         return new Group(-1, "Groupless", "Members without a group", 0, allUsers);
     }
 
-    public Group getTeacherGroup(){
+    protected Group getTeacherGroup(){
         Set<User> allUsers = getAllUsers();
 
         return new Group(-2, "Teaching staff", "Members with role teacher", 0, allUsers);
@@ -65,20 +65,22 @@ public class GroupsController {
     protected Set<User> getAllUsers() {
         Set<User> users = new HashSet<>();
         int offset = 0;
-        int limit = 50;
         int sizeOfSet = 1;
 
         while (sizeOfSet > 0) {
-            UserListResponse userListResponse = userAccountClientService.getPaginatedUsers(offset, limit, "username", true);
+            UserListResponse userListResponse = userAccountClientService.getPaginatedUsers(offset, 50, "userId", true);
             List<User> returnedUsers = userListResponse.getUsers();
-            for(User user: returnedUsers){
-                users.add(user);
-            }
             sizeOfSet = userListResponse.getResultSetSize();
-            offset += 50;
-            limit += 50;
-        }
 
+            if (sizeOfSet > 0){
+                users.addAll(returnedUsers);
+            }
+            offset += 50;
+
+            if (sizeOfSet < 50){
+                sizeOfSet = 0;
+            }
+        }
         return users;
     }
 }
