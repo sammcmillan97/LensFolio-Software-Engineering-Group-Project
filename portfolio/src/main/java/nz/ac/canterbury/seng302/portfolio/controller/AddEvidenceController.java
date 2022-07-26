1c870ea54906199e4445ca096e3a406a8bb477bc
Luke Garside <lcg37@uclive.ac.nz>
2022-07-27 00:23:47 +1200

    #feature Created basic add evidence page.

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/AddEvidenceController.java
@@ -0,0 +1,38 @@
+package nz.ac.canterbury.seng302.portfolio.controller;
+
+import nz.ac.canterbury.seng302.portfolio.model.User;
+import nz.ac.canterbury.seng302.portfolio.service.UserAccountClientService;
+import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * The controller for handling backend of the add evidence page
+ */
+@Controller
+public class AddEvidenceController {
+
+    @Autowired
+    private UserAccountClientService userService;
+
+    /**
+     * Display the add evidence page.
+     * @param principal Authentication state of client
+     * @param model Parameters sent to thymeleaf template to be rendered into HTML
+     * @return The add evidence page.
+     */
+    @GetMapping("/add-evidence")
+    public String portfolio(
+            @AuthenticationPrincipal AuthState principal,
+            Model model
+    ) {
+        User user = userService.getUserAccountByPrincipal(principal);
+        model.addAttribute("user", user);
+        return "addEvidence";
+    }
+
+}
+
