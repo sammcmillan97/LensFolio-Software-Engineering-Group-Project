bfc42b785f6b238303913a3073a9a643f9ff038b
Luke Garside <lcg37@uclive.ac.nz>
2022-07-27 11:35:57 +1200

    #feature Added controller method for saving a piece of evidence. Now only the frontend is needed (hopefully!)

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/AddEvidenceController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/AddEvidenceController.java
@@ -1,6 +1,9 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
+import nz.ac.canterbury.seng302.portfolio.model.Evidence;
 import nz.ac.canterbury.seng302.portfolio.model.User;
+import nz.ac.canterbury.seng302.portfolio.service.EvidenceService;
+import nz.ac.canterbury.seng302.portfolio.service.PortfolioUserService;
 import nz.ac.canterbury.seng302.portfolio.service.UserAccountClientService;
 import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,10 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
 
 /**
  * The controller for handling backend of the add evidence page
@@ -18,6 +25,12 @@ public class AddEvidenceController {
     @Autowired
     private UserAccountClientService userService;
 
+    @Autowired
+    private PortfolioUserService portfolioUserService;
+
+    @Autowired
+    private EvidenceService evidenceService;
+
     /**
      * Display the add evidence page.
      * @param principal Authentication state of client
@@ -25,13 +38,42 @@ public class AddEvidenceController {
      * @return The add evidence page.
      */
     @GetMapping("/add-evidence")
-    public String portfolio(
+    public String addEvidence(
             @AuthenticationPrincipal AuthState principal,
             Model model
     ) {
         User user = userService.getUserAccountByPrincipal(principal);
         model.addAttribute("user", user);
         return "addEvidence";
+    }
+
+    /**
+     * Save a piece of evidence. It will be rejected silently if the data given is invalid, otherwise it will be saved.
+     * If saved, the user will be taken to their portfolio page.
+     * @param principal Authentication state of client
+     * @param title The title of the piece of evidence
+     * @param description The description of the piece of evidence
+     * @param date The date the evidence occured
+     * @param model Parameters sent to thymeleaf template to be rendered into HTML
+     * @return A redirect to the portfolio page, or staying on the add evidence page
+     */
+    @PostMapping("/add-evidence")
+    public String saveEvidence(
+            @AuthenticationPrincipal AuthState principal,
+            @RequestParam(name="title") String title,
+            @RequestParam(name="description") String description,
+            @RequestParam(name="date") java.sql.Date date,
+            Model model
+    ) {
+        int userId = userService.getUserId(principal);
+        int projectId = portfolioUserService.getUserById(userId).getCurrentProject();
+        Evidence evidence = new Evidence(userId, projectId, title, description, date);
+        try {
+            evidenceService.saveEvidence(evidence);
+        } catch (IllegalArgumentException exception) {
+            return "addEvidence"; //Fail silently as client has responsibility for error checking
+        }
+        return "redirect:/portfolio";
     }
 
 }
