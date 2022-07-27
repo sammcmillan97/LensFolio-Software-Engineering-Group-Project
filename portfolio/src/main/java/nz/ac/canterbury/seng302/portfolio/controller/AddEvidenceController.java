550680eecac1ea082e67504e8c90eaa7d7cd061d
Luke Garside <lcg37@uclive.ac.nz>
2022-07-27 16:06:24 +1200

    #test Added unit tests for the evidence controller.

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/AddEvidenceController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/AddEvidenceController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -21,6 +23,8 @@ import java.util.Date;
  */
 @Controller
 public class AddEvidenceController {
+
+    private static final String ADD_EVIDENCE = "addEvidence";
 
     @Autowired
     private UserAccountClientService userService;
@@ -44,7 +48,7 @@ public class AddEvidenceController {
     ) {
         User user = userService.getUserAccountByPrincipal(principal);
         model.addAttribute("user", user);
-        return "addEvidence";
+        return ADD_EVIDENCE;
     }
 
     /**
@@ -53,7 +57,7 @@ public class AddEvidenceController {
      * @param principal Authentication state of client
      * @param title The title of the piece of evidence
      * @param description The description of the piece of evidence
-     * @param date The date the evidence occured
+     * @param dateString The date the evidence occurred, in yyyy-MM-dd string format
      * @param model Parameters sent to thymeleaf template to be rendered into HTML
      * @return A redirect to the portfolio page, or staying on the add evidence page
      */
@@ -62,16 +66,24 @@ public class AddEvidenceController {
             @AuthenticationPrincipal AuthState principal,
             @RequestParam(name="title") String title,
             @RequestParam(name="description") String description,
-            @RequestParam(name="date") java.sql.Date date,
+            @RequestParam(name="date") String dateString,
             Model model
     ) {
+        User user = userService.getUserAccountByPrincipal(principal);
+        model.addAttribute("user", user);
+        Date date;
+        try {
+            date = new SimpleDateFormat("yyyy-MM-dd").parse(dateString);
+        } catch (ParseException exception) {
+            return ADD_EVIDENCE; // Fail silently as client has responsibility for error checking
+        }
         int userId = userService.getUserId(principal);
         int projectId = portfolioUserService.getUserById(userId).getCurrentProject();
         Evidence evidence = new Evidence(userId, projectId, title, description, date);
         try {
             evidenceService.saveEvidence(evidence);
         } catch (IllegalArgumentException exception) {
-            return "addEvidence"; //Fail silently as client has responsibility for error checking
+            return ADD_EVIDENCE; // Fail silently as client has responsibility for error checking
         }
         return "redirect:/portfolio";
     }
