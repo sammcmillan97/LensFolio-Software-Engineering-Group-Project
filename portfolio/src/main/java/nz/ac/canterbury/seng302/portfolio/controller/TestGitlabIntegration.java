1eaa00473e812018672f879aacd9c06fc0b30b1f
Bede Skinner-Vennell <bjs185@uclive.ac.nz>
2022-07-28 21:59:57 +1200

    #feature tested gitlab4j with gitlab api

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/TestGitlabIntegration.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/TestGitlabIntegration.java
@@ -1,29 +1,25 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
+import org.gitlab4j.api.GitLabApi;
+import org.gitlab4j.api.GitLabApiException;
+import org.gitlab4j.api.models.Commit;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
 public class TestGitlabIntegration {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws GitLabApiException {
         String TOKEN = System.getenv("GITLAB_PROJECT_TOKEN");
         String PROJECT_ID = "13529";
-        URL url = new URL("https://eng-git.canterbury.ac.nz/api/v4/projects/" + PROJECT_ID + "/repository/commits");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestProperty("PRIVATE-TOKEN", TOKEN);
+        String GITLAB_URL = "https://eng-git.canterbury.ac.nz";
 
-        int status = con.getResponseCode();
-        System.out.println(status);
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer content = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
-        }
-        in.close();
-        System.out.println(content);
+        GitLabApi gitLabApi = new GitLabApi(GITLAB_URL, TOKEN);
+
+        List<Commit> commits = gitLabApi.getCommitsApi().getCommits(PROJECT_ID);
+        System.out.println(commits);
     }
 }
