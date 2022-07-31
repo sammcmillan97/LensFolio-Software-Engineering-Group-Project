a3bd9aa5c4ebeedbb1b69504464f9161870a21e6
Danish Jahangir <dkj23@uclive.ac.nz>
2022-07-31 15:23:58 +1200

    #refactor Removed evidence.js as it was unnecessary

--- portfolio/src/main/resources/static/scripts/evidence.js
+++
@@ -1,8 +0,0 @@
-/**
- * Takes the evidence html element and makes it expanded
- * @param evidence html element
- */
-function expandEvidence(evidence) {
-    evidence.getElementsByClassName("evidence__details collapse")[0].className = "evidence__details collapse show"
-    evidence.getElementsByClassName("evidence_title_child")[0].setAttribute("aria-expanded", "true")
-}
