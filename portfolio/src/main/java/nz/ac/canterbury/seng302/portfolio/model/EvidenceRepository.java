85a18649a52049807256ac1389140bf45f99043e
Bede Skinner-Vennell <bjs185@uclive.ac.nz>
2022-08-06 00:33:28 +1200

    #chore removed unused imports

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/EvidenceRepository.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/EvidenceRepository.java
@@ -1,6 +1,5 @@
 package nz.ac.canterbury.seng302.portfolio.model;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
