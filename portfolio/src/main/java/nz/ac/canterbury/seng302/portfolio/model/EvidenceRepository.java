68a7e1f36119c9fc3134b994259758fb43797dfa
Amy Sloane <ajs418@uclive.ac.nz>
2022-08-05 12:53:49 +1200

    #Fix Remove unused imports

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/EvidenceRepository.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/EvidenceRepository.java
@@ -1,8 +1,6 @@
 package nz.ac.canterbury.seng302.portfolio.model;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
