0d512f8542439199906f60f13f3794e40c87bcdd
Bede Skinner-Vennell <bjs185@uclive.ac.nz>
2022-08-05 23:45:49 +1200

    #feature added skills page that shows all of the users evidence with the given skill

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/WebConfig.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @Configuration
 public class WebConfig implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/SkillsController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/SkillsController.java
@@ -1,7 +1,9 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
 import nz.ac.canterbury.seng302.portfolio.model.Evidence;
+import nz.ac.canterbury.seng302.portfolio.model.User;
 import nz.ac.canterbury.seng302.portfolio.service.EvidenceService;
+import nz.ac.canterbury.seng302.portfolio.service.UserAccountClientService;
 import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -12,23 +14,81 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * The controller for handling backend of the skills page
  */
 @Controller
 public class SkillsController {
+
+    @Autowired
+    private UserAccountClientService userService;
     @Autowired
     EvidenceService evidenceService;
 
-    @GetMapping("/portfolio/skill")
+    /**
+     * Finds the logged in user's id and then loads the evidence page using the more generic endpoint
+     * @param principal Authentication state of client
+     * @param skill The skill to search for
+     * @param model Parameters sent to thymeleaf template to be rendered into HTML
+     * @return A page with all of the user's evidence with the given skill
+     */
+    @GetMapping("/portfolio-skill")
     public String getEvidenceWithSkill(
                                         @AuthenticationPrincipal AuthState principal,
                                         @RequestParam("skill") String skill,
                                         Model model) {
-        List<Evidence> evidence = evidenceService.retrieveEvidenceBySkill(skill);
+        User user = userService.getUserAccountByPrincipal(principal);
+        model.addAttribute("user", user);
+        model.addAttribute("pageUser", user);
+        model.addAttribute("owner", true);
 
+        int userId = user.getId();
+
+        List<Evidence> evidenceList = evidenceService.retrieveEvidenceBySkillAndUser(skill, userId);
+        model.addAttribute("evidenceList", evidenceList);
+
+        String skillName = skill.replace("_", " ");
+        model.addAttribute("skillName", skillName);
 
         return "skills";
+    }
+
+    /**
+     * Fetches all evidence with the given skill and user and return a page with it in.
+     * @param principal Authentication state of client
+     * @param userId The id of the user whose evidence needs to be searched
+     * @param skill The skill to search for
+     * @param model Parameters sent to thymeleaf template to be rendered into HTML
+     * @return A page with all of the user's evidence with the given skill
+     */
+    @GetMapping("/portfolio-{userId}-skill")
+    public String getOtherUsersEvidenceWithSkill(
+            @AuthenticationPrincipal AuthState principal,
+            @PathVariable("userId") int userId,
+            @RequestParam("skill") String skill,
+            Model model) {
+
+        User user = userService.getUserAccountByPrincipal(principal);
+        model.addAttribute("user", user);
+        User pageUser = userService.getUserAccountById(userId);
+
+        List<Evidence> evidenceList = evidenceService.retrieveEvidenceBySkillAndUser(skill, userId);
+
+        String skillName = skill.replace("_", " ");
+        model.addAttribute("skillName", skillName);
+        model.addAttribute("user", user);
+        model.addAttribute("pageUser", pageUser);
+        model.addAttribute("evidenceList", evidenceList);
+
+        if (Objects.equals(pageUser.getUsername(), "")) {
+            return "redirect:/profile";
+        } else if (user.getId() == pageUser.getId()) {
+            return "redirect:/portfolio/skill?skill=" + skill; // Take user to their own portfolio if they try to view it
+        } else {
+            model.addAttribute("owner", false);
+            return "skills";
+        }
     }
 }
