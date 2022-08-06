c4b24aa383a98f70d17810bf4e5a7c0007495f59
Bede Skinner-Vennell <bjs185@uclive.ac.nz>
2022-08-06 21:23:17 +1200

    #feature made the planner update without refreshing
    Added methods to planner controller and planner javascript. Removed some print statements from the PlannerUtil class

--- portfolio/src/main/resources/static/scripts/projectDetails.js
+++ portfolio/src/main/resources/static/scripts/projectDetails.js
@@ -44,9 +44,15 @@ setTimeout(function () {
         editPolling();
     }, 1000);
 
+
+/**
+ * Takes note of the currently active tab/carousel on the page, fetches updated data from the controller
+ * then updates the page and keeps the user on the same tab/carousel
+ */
 async function updatePage() {
     const currentProjectId = document.getElementById("projectId").textContent;
 
+    // Figure out the current active tab/carousel
     let currentCarouselIndex = 0;
     const carouselItems = document.getElementsByClassName("carousel-item")
     for (let i = 0; i < carouselItems.length; i++) {
@@ -55,20 +61,24 @@ async function updatePage() {
             break
         }
     }
-    const newPage = document.createElement('html')
 
-      newPage.innerHTML = await fetch("projectDetails-" + currentProjectId).then((response) => {
+    // Fetch the new page data from the controller
+    const newPage = document.createElement('html')
+    newPage.innerHTML = await fetch("projectDetails-" + currentProjectId).then((response) => {
         return response.text()
     })
 
+    // Unmark the default active tab/carousel (if it's not the required one)
+    // Mark the required tab/carousel as active (if it's not already)
     const newCarouselItems = newPage.getElementsByClassName("carousel-item")
     for (let i = 0; i < newCarouselItems.length; i++) {
         if (newCarouselItems[i].className.includes("active") && i !== currentCarouselIndex) {
             newCarouselItems[i].className = "carousel-item"
-        } else if (i === currentCarouselIndex) {
+        } else if (!newCarouselItems[i].className.includes("active") && i === currentCarouselIndex) {
             newCarouselItems[i].classList.add("active")
         }
     }
 
+    // Update the page with the new data
     document.body.innerHTML = newPage.innerHTML
 }
