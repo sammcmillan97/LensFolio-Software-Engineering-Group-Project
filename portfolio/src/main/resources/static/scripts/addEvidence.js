b987efd6b1d6c19c29620e21c8bb0a238ad4bd13
Luke Garside <lcg37@uclive.ac.nz>
2022-08-07 17:07:38 +1200

    #feature #pair[lcg37, bsa57] Added skills to a list to prepare adding them to tags.

--- portfolio/src/main/resources/static/scripts/addEvidence.js
+++ portfolio/src/main/resources/static/scripts/addEvidence.js
@@ -6,22 +6,46 @@ function checkEmpty() {
         || document.getElementById("evidence-form__description-field").value.length > 1024;
 }
 
+let skillList = []
+
+function addToSkills(skill) {
+    if (!skillList.includes(skill)) {
+        skillList.push(skill);
+    }
+}
+
+function removeLastSkill() {
+    skillList.pop();
+}
+
+function removeSkill(skill) {
+    skillList.splice(skillList.indexOf(skill), 1);
+}
+
 document.getElementById("evidence-form__skills-field").addEventListener("input", (event) => {
     let value = event.target.value;
     let skills = value.split(" ");
     let lastSkill = skills.pop();
+    let shouldUpdateSkills = false;
     for (const skill of skills) {
         if (skill !== "") {
-            console.log("add " + skill + " to DOM, nice job!")
+            shouldUpdateSkills = true;
+            addToSkills(skill);
+            console.log("add " + skill + " to DOM, nice job!");
         }
     }
+    lastSkill = lastSkill.slice(0, 50);
     document.getElementById("evidence-form__skills-field").value = lastSkill;
+    if (shouldUpdateSkills) {
+        updateTagsInDOM(skillList);
+    }
 })
 
 document.getElementById("evidence-form__skills-field").addEventListener("keydown", (event) => {
     let skillText = event.target.value
     if (event.key === "Backspace" && skillText === "") {
-        console.log("destroy tag if one exists")
+        removeLastSkill();
+        updateTagsInDOM(skillList);
     }
 })
 
