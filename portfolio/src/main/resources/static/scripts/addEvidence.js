2d276937c942bf133b747fdc24c609fd92095764
Luke Garside <lcg37@uclive.ac.nz>
2022-08-07 16:23:52 +1200

    #feature #pair[lcg37, bsa57] Added logic for tags to skills entry box. The tags themselves do not yet appear.

--- portfolio/src/main/resources/static/scripts/addEvidence.js
+++ portfolio/src/main/resources/static/scripts/addEvidence.js
@@ -6,6 +6,21 @@ function checkEmpty() {
         || document.getElementById("evidence-form__description-field").value.length > 1024;
 }
 
-document.getElementById("evidence-form__skills-field").addEventListener("keydown", (event) =>{
-    console.log(event);
+document.getElementById("evidence-form__skills-field").addEventListener("input", (event) => {
+    let value = event.target.value;
+    let skills = value.split(" ");
+    let lastSkill = skills.pop();
+    for (const skill of skills) {
+        if (skill !== "") {
+            console.log("add " + skill + " to DOM, nice job!")
+        }
+    }
+    document.getElementById("evidence-form__skills-field").value = lastSkill;
+})
+
+document.getElementById("evidence-form__skills-field").addEventListener("keydown", (event) => {
+    let skillText = event.target.value
+    if (event.key === "Backspace" && skillText === "") {
+        console.log("destroy tag if one exists")
+    }
 })
