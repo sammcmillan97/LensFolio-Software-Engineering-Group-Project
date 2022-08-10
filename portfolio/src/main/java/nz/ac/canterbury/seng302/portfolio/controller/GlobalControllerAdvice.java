6815a2b5cf33f61720e51daed0345408cd753806
Amy Sloane <ajs418@uclive.ac.nz>
2022-08-10 16:12:45 +1200

    #Fix Added 0 to title regex

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GlobalControllerAdvice.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GlobalControllerAdvice.java
@@ -27,8 +27,8 @@ public class GlobalControllerAdvice extends ResponseEntityExceptionHandler {
     @Autowired
     private UserAccountClientService userAccountClientService;
 
-    private static final String REGEX_WITH_SPACE = "[a-zA-Z1-9àáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆŠŽ∂ð,. '\\-]";
-    private static final String REGEX_WITHOUT_SPACE = "[a-zA-Z1-9àáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆŠŽ∂ð,.'\\-]";
+    private static final String REGEX_WITH_SPACE = "[a-zA-Z0-9àáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆŠŽ∂ð,. '\\-]";
+    private static final String REGEX_WITHOUT_SPACE = "[a-zA-Z0-9àáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆŠŽ∂ð,.'\\-]";
 
     @ModelAttribute("allProjects")
     public List<Project> getAllProjects(){
@@ -74,6 +74,11 @@ public class GlobalControllerAdvice extends ResponseEntityExceptionHandler {
         }
     }
 
+    /**
+     * This regex will reject blanks, and emojis,
+     * but still allow specific characters and numbers
+     * @return regex
+     */
     @ModelAttribute("titlePattern")
     public String getTitlePattern(){
         return "(" + REGEX_WITH_SPACE + "*)(" + REGEX_WITHOUT_SPACE + ")("  + REGEX_WITH_SPACE + "*)";
